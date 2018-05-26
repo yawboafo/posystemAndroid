0964be4ed645fbf3107bd23c75c5120ed221c027
nykb <koranteng@smsgh.com>
2018-05-26 17:19:50 +0100

    framework init

---
+++ app/src/main/java/com/tech/tle/posystemandroid/Application.java
@@ -0,0 +1,32 @@
+package com.tech.tle.posystemandroid;
+
+/**
+ * Created by nykbMac on 26/05/2018.
+ */
+
+import android.text.TextUtils;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class Application extends android.app.Application{
+
+
+
+
+
+    private static Application mInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+    }
+
+    public static synchronized Application getInstance() {
+        return mInstance;
+    }
+
+
+}
